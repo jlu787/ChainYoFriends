c91eddb52b43cd213a08d5efcb6b4d4e8ce7a2cc
dylHall <dylan.hall077@gmail.com>
2019-07-20 20:57:03 +1200

    cleaned up bunny add mechanics

--- ChainYoFriends/Assets/Scripts/DumbFukChainManager.cs
+++ ChainYoFriends/Assets/Scripts/DumbFukChainManager.cs
@@ -7,47 +7,49 @@ public class DumbFukChainManager : MonoBehaviour
 {
 
     public List<GameObject> bunnyChain;
+    public int currentChainLength = 2;
     public GameObject bunny;
     public float frequency = 0.5f;
-    public float spawnOffset = 0.5f;
+    public float bunnyMass = 1f;
+    public float bunnyLinearDrag = 0.05f;
+    public float bunnyAngularDrag = 0f;
+
     private float counter = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = currentChainLength; i<bunnyChain.Count; i++)
+        {
+            enableBunny(i, false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currentChainLength == bunnyChain.Count)
+            return;
+
         counter += Time.deltaTime;
-        if (counter >= 1f/frequency && bunnyChain.Count < 10)
+        if (counter >= 1f/frequency)
         {
             counter = 0f;
-            Add();
+            AddToChain();
         }
     }
 
-    void Add()
+    void AddToChain()
     {
-        GameObject lastBunny = bunnyChain.Last();
-        GameObject secondToLastBunny = lastBunny;
-        if (bunnyChain.Count > 2)
-        {
-            secondToLastBunny = bunnyChain[bunnyChain.Count - 1];
-        }
-        Debug.Log(Vector2.Angle(lastBunny.transform.right, secondToLastBunny.transform.right));
-        
-        Vector3 newBunnyPos = lastBunny.transform.position + lastBunny.transform.right * spawnOffset;
-        GameObject newBunny = Instantiate(bunny, newBunnyPos, Quaternion.identity) as GameObject;
-        HingeJoint2D hj = newBunny.GetComponent<HingeJoint2D>();
-        hj.connectedBody = bunnyChain.Last().GetComponent<Rigidbody2D>();
-        JointAngleLimits2D limits = hj.limits; 
-        limits.max = lastBunny.GetComponent<HingeJoint2D>().limits.max + 30;
-        limits.min = lastBunny.GetComponent<HingeJoint2D>().limits.min - 30;
-        hj.limits = limits;
-        bunnyChain.Add(newBunny); //probably
-        newBunny.transform.rotation = lastBunny.transform.rotation;
+        enableBunny(currentChainLength, true);
+        currentChainLength++;
+    }
+
+    private void enableBunny(int index, bool state)
+    {
+        Debug.Log("Enabling bunny: " + index);
+        GameObject bunny = bunnyChain[index];
+        SpriteRenderer render = bunny.GetComponent<SpriteRenderer>();
+        render.enabled = state;
     }
 }
