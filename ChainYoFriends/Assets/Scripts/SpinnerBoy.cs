405e9e2715047f21db970edf009aed054b5f9deb
Arran Davis <adav194@aucklanduni.ac.nz>
2019-07-21 16:18:23 +1200

    Add Chainsaw sprits

---
+++ ChainYoFriends/Assets/Scripts/SpinnerBoy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerBoy : MonoBehaviour
+{
+    public float revspeed = 10f;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f, revspeed * Time.deltaTime);
+    }
+}
