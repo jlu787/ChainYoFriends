9fce42907f20d4f4c22f4ec199e0ba1422fc2443
Daniel Bedggood <ddbedggood@gmail.com>
2019-07-21 13:00:21 +1200

    Start and End screens

---
+++ ChainYoFriends/Assets/GameManagerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class GameManagerScript : MonoBehaviour
+{
+    public float roundDuration;
+    private float timeLeft;
+
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI playerOneScore;
+    public TextMeshProUGUI playerTwoScore;
+
+    public DumbFukChainManager playerOneDFCM;
+    public DumbFukChainManager playerTwoDFCM;
+
+    public GameObject gameEndPanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (roundDuration > 0)
+        {
+            roundDuration -= Time.deltaTime;
+            timeLeft = Mathf.Round(roundDuration);
+            timerText.text = timeLeft.ToString("0");
+            playerOneScore.text = playerOneDFCM.currentChainLength.ToString();
+            playerTwoScore.text = playerTwoDFCM.currentChainLength.ToString();
+        } else
+        {
+            timerText.text = "GAME END";
+            gameEndPanel.SetActive(true);
+        }
+    }
+}
